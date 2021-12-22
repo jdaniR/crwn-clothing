4037b283a4ce0f4a8ac7cede4b358f75054f5a75
j.dani <janki@innovactdigital.com>
2021-12-22 14:58:21 +0000

    Task-4.5: Add Sass styles for homepage menu items

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,41 +1,42 @@
 import React from "react";
-//#endregion
-//#endregion
+import "./homepage.styles.scss";
 
-const HomePage = () => (
-  <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Hats</h1>
-          <span className="subtitle">SHOP NOW</span>
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Jackets</h1>
-          <span className="subtitle">SHOP NOW</span>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Sneakers</h1>
-          <span className="subtitle">SHOP NOW</span>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Sneakers</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Mans</h1>
-          <span className="subtitle">SHOP NOW</span>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Mans</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Womens</h1>
-          <span className="subtitle">SHOP NOW</span>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Womens</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 export default HomePage;
