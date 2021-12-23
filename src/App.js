c6492728fe7ecaa2b533929a5f6a926a6fd0c9af
j.dani <janki@innovactdigital.com>
2021-12-23 15:16:21 +0000

    Task-5.2:  Add a simple functional component, add routes for it, use speed operator and use a hook to navigate.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/hats" element={<HatsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,25 @@
+//https://stackoverflow.com/questions/69934351/withrouter-is-not-exported-from-react-router-dom/69934614
 import React from "react";
-//#endregion
-import "./menu-item.styles.scss";
-//#endregion
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  const navigate = useNavigate();
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`hats`)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
