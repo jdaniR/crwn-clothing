3d5219013973166f016e6c56dec80791adf2a2eb
j.dani <janki@innovactdigital.com>
2021-12-23 15:35:31 +0000

    Task-6.1:  Add a new page component for Shop, add a route for shop, assign shop master data from json to state.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shoppage.component";
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route path="/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
       </BrowserRouter>
     </div>
