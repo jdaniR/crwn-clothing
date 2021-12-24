559228ee7a24f3a7b8951946555233413559f241
j.dani <janki@innovactdigital.com>
2021-12-24 10:45:28 +0000

    Task-6.4: Add a header component , style it and use it in App.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
