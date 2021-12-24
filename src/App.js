ce32dca171879b94c654a5b030c158305eb17938
j.dani <janki@innovactdigital.com>
2021-12-24 11:47:41 +0000

    Task-6.5: Set up a Sign in / Sign up Page component and a SingIn form comp, add event handlers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
         </Routes>
       </BrowserRouter>
     </div>
