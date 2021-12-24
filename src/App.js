0f5a8878910f55eaef6c803d1a066cc91f5a4892
j.dani <janki@innovactdigital.com>
2021-12-24 10:14:17 +0000

    Task-6.3: Add a new functional component to render collection item details.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shoppage/shoppage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
