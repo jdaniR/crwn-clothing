6b8f8448615cad6d774d488937bb08ce83b9308e
j.dani <janki@innovactdigital.com>
2021-12-29 12:56:58 +0000

    Task-7.4: Conditionally render links for sign in and sign out links in header comp based on user obj

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
     return (
       <div>
         <BrowserRouter>
-          <Header />
+          <Header currentUser={this.state.currentUser} />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route path="/shop" element={<ShopPage />} />
