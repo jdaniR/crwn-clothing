7c8cee6ad18bcb406709cd145ca9f1f8bab0cd4f
j.dani <janki@innovactdigital.com>
2021-12-30 15:14:15 +0000

    Task-7.7: If Snapshot doesn't exist, then create a new user document and insert into firebase.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,12 +15,27 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc("users/124hdskjahfkjlahs");
-  console.log(userRef);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  console.log(snapShot);
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
