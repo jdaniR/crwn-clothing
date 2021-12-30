3250b715a084e6416d6c57cf3610c018a5404438
j.dani <janki@innovactdigital.com>
2021-12-30 12:43:18 +0000

    Task-7.6: Send and log user document in firebase if user authentication is not null.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,17 @@ const config = {
   measurementId: "G-J6PVTENYDG",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc("users/124hdskjahfkjlahs");
+  console.log(userRef);
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
