88cc7336dfc490692389f67166d1f7c21063bcc5
j.dani <janki@innovactdigital.com>
2022-01-06 20:07:22 +0000

    Task-9.12: Implement a selectors cart item count and cart items, to improve performance using memoization (cache) with ‘reselect' lib

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors.js";
+
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
@@ -17,10 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
-    0
-  ),
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
