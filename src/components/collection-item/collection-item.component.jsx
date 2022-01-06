3e8421d7db745b1f96e92eab1fe5475355da1bca
j.dani <janki@innovactdigital.com>
2022-01-06 10:32:17 +0000

    Task-9.5: Add ‘Add to cart’ button to the shop collection items and style them.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./collection-item.styles.scss";
 
@@ -15,6 +16,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton invert> Add to Cart </CustomButton>
   </div>
 );
 
