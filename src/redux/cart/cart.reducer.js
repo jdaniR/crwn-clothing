e0b3b6b78117963d5a59a3e5a7effef38d60d543
j.dani <janki@innovactdigital.com>
2022-01-06 12:36:25 +0000

    Issue: Resolved an error related to undefined property

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -16,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.paylod),
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
