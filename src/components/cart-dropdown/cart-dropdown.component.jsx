93b80e56082de8ca0474bc9992e37982b3ff2c69
j.dani <janki@innovactdigital.com>
2022-01-10 22:30:07 +0000

    Task-10.3: Trigger a toggle the hidden state of the Cart Dropdown on button click.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,10 +7,11 @@ import { useNavigate } from "react-router-dom";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, dispatch }) => {
   let navigate = useNavigate();
   return (
     <div className="cart-dropdown">
@@ -23,7 +24,12 @@ const CartDropdown = ({ cartItems }) => {
           <span className="empty-message">Your Cart is Empty!</span>
         )}
       </div>
-      <CustomButton onClick={() => navigate("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          navigate("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
