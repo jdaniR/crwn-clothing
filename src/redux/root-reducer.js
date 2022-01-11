612d0f94716a48c8e90ee5b495fc7a4f0e010b26
j.dani <janki@innovactdigital.com>
2022-01-11 14:46:56 +0000

    Task-12.1: Move Directory state into store so that we can use it at multiple touch points.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducer";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
@@ -14,5 +15,6 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
