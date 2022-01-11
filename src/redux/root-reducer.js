a32336fd6828b3718d5fc50cb3914920bbff3ecf
j.dani <janki@innovactdigital.com>
2022-01-11 19:49:10 +0000

    Task-12.1: Move out Shop state into store so that we can use it at multiple touch points.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
@@ -16,5 +17,6 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
