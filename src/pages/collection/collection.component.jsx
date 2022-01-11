d579ac0737f9a1366465b47528240cfb46134a57
j.dani <janki@innovactdigital.com>
2022-01-11 22:22:10 +0000

    Task-13.5:Memoizing selectCollection and collectionUrlParam

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selector";
 
 import "./collection.styles.scss";
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -15,9 +16,10 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-export const selectCollection = (collectionUrlParam) =>
+export const selectCollection = memoize((collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
     collections.find(
       (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     )
-  );
+  )
+);
