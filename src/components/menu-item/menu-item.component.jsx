d9606f988115b104a8770daeb2bb3340d2f3c5f0
j.dani <janki@innovactdigital.com>
2022-01-12 11:18:01 +0000

    Task-13.6: Style collection component and set urls for collection items page.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
   const navigate = useNavigate();
   return (
-    <div className={`${size} menu-item`} onClick={() => navigate(`hats`)}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         className="background-image"
         style={{
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,16 +4,22 @@ import { useSelector } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop.selector";
 
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import "./collection.styles.scss";
 
 const CollectionPage = () => {
   const { collectionId } = useParams();
   const collection = useSelector(selectCollection(collectionId));
-  console.log(collection);
-
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>Category Page</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>         
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
