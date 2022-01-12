ec1a2f5d52c487c693f6dd3b76e8ab666e2f0a5d
j.dani <janki@innovactdigital.com>
2022-01-12 13:27:23 +0000

    Task-14.3: Update exp date for the test card to current year in checkout page.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -39,7 +39,8 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="test-warning">
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - {new Date().getMonth() + 2}/
+      {new Date().getFullYear() + 1}- CVV: 123
     </div>
     <StripeCheckoutButton price={total} />
   </div>
