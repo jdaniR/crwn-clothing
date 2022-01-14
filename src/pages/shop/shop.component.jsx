e25312608729a4e1d81fd337a0014ad8b6df31bf
j.dani <janki@innovactdigital.com>
2022-01-14 10:04:33 +0000

    Task-17.3: Convert Shop Page functional component into a class component.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,13 +6,17 @@ import CollectionOverview from "../../components/collection-overview/collection-
 
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => (
-  <div className="shop-page">
-    <Routes>
-      <Route exact path="/*" element={<CollectionOverview />} />
-      <Route exact path="/:collectionId" element={<CollectionPage />} />
-    </Routes>
-  </div>
-);
+class ShopPage extends React.component {
+  render() {
+    return (
+      <div className="shop-page">
+        <Routes>
+          <Route exact path="/*" element={<CollectionOverview />} />
+          <Route exact path="/:collectionId" element={<CollectionPage />} />
+        </Routes>
+      </div>                             
+    );
+  }
+}
 
 export default ShopPage;
