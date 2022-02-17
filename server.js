4aca4606a4406c58e3f6d70da543d9cc93a915ae
j.dani <janki@innovactdigital.com>
2022-02-17 10:41:36 +0000

    Task-27.5: Write a payment route handler in server to post payment info to Stripe. Lesson: 246

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+//set up up a node server
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,7 +21,23 @@ app.get('*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(port, error => {
-    if (error) throw error;
-    console.log('server running on port' + port);
+app.listen(port, (error) => {
+	if (error) throw error;
+	console.log('server running on port' + port);
+});
+
+app.post('/payment', (req, res) => {
+	const body = {
+		source: req.nody.token.id,
+		amount: req.body.amount,
+		currency: 'gbp',
+	};
+
+	stripe.charges.create(body, (stripeErr, stripeRes) => {
+		if (stripeerr) {
+			res.status(500).send({ error: stripeErr });
+		} else {
+			status(200).send({ success: stripeRes });
+		}
+	});
 });
