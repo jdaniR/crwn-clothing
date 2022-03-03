95e92fc00761878e496c5c4d90c084d6fb42f2d2
j.dani <janki@innovactdigital.com>
2022-03-03 20:29:47 +0000

    Task-28.1: Add a global style component and add a @media query tag for padding.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,40 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import "./App.css";
-
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import CheckoutPage from "./pages/checkout/checkout.component";
-import { selectCurrentUser } from "./redux/user/user.selectors";
-import { checkUserSession } from "./redux/user/user.actions";
+import { GlobalStyle } from './global.styles';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 const App = () => {
-  const currentUser = useSelector(selectCurrentUser);
-  const dispatch = useDispatch();
+	const currentUser = useSelector(selectCurrentUser);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, [dispatch]);
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/shop/*" element={<ShopPage />} />
-          <Route exact path="/checkout" element={<CheckoutPage />} />
-          <Route
-            path="/signIn"
-            element={
-              currentUser ? (
-                <Navigate replace to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	useEffect(() => {
+		dispatch(checkUserSession());
+	}, [dispatch]);
+	return (
+		<div>
+			<GlobalStyle />
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route exact path="/" element={<HomePage />} />
+					<Route path="/shop/*" element={<ShopPage />} />
+					<Route exact path="/checkout" element={<CheckoutPage />} />
+					<Route
+						path="/signIn"
+						element={currentUser ? <Navigate replace to="/" /> : <SignInAndSignUpPage />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
