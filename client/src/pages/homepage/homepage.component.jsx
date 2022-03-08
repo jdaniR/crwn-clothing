cb868780f89a21d3efb4c6dc8c74719b8a26e57f
j.dani <janki@innovactdigital.com>
2022-03-08 12:07:59 +0000

    Task-29.8: Use Performance API to log info for Directory component

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -9,7 +9,18 @@ import { HomePageContainer } from './homepage.styles';
 function HomePage() {
 	return (
 		<HomePageContainer>
-			<Directory />
+			<Profiler
+				id="Directory"
+				onRender={(id, phase, actualDuration) => {
+					console.log({
+						id,
+						phase,
+						actualDuration,
+					});
+				}}
+			>
+				<Directory />
+			</Profiler>
 		</HomePageContainer>
 	);
 }
